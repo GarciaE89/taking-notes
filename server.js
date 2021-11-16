8cffddc0fdfbfc780a5bbe6ea515e873e61349b0
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-11-15 22:34:46 -0700

    added server code

--- server.js
+++ server.js
@@ -5,3 +5,23 @@ const PORT = process.env.PORT || 3001;
 
 const app = express;
 
+// route modules trying to organize by concerns
+const apiRoutes = require('./routes/api/apiRoutes');
+const htmlRoutes = require('./routes/html/htmlRoutes');
+
+// middleware serves static content from public dir.
+app.use(express.static('public'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+//listener for api's
+app.listen(PORT, () => {
+  console.log(`API server listening on ${PORT}!`);
+});
