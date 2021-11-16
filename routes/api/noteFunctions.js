ae83680380fcf0dcc0ac84061b3aa672b1754858
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-11-15 23:32:19 -0700

    added functions to notes module

--- routes/api/noteFunctions.js
+++ routes/api/noteFunctions.js
@@ -2,11 +2,51 @@
 const fs = require('fs');
 const path = require('path');
 
-function findNotebyId()
+function findNotebyId(id, notesArr) {
+    let newNoteArr = notesArr;
+    let deleteNote = id;
+    // remove deleted note from array
+    newNoteArr = newNoteArr.filter(notes => {
+        return notes.id != deleteNote;
+    })
 
-function createNote()
+    // rewrite new array
+fs.writeFileSync(path.join(__dirname, '../../db/db.json')),
+JSON.stringify(newNoteArr, null, 2);
+return newNoteArr;
+    
+};
 
-function validateNotes()
+
+
+function createNote(body, notesArr) {
+    let note = body;
+    let allNotes = notesArr;
+    allNotes.push(note);
+
+    // write/update to db.json file
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(notesArr, null, 2)
+    );
+
+    return notesArr;
+};
+
+function validateNotes(note) {
+    if (!note.title) {
+        return false;
+      }
+      if (!note.text) {
+        return false;
+      }
+      if (!note.id) {
+        return false;
+      }
+      return true;
+    
+
+}
 
 module.exports = {
     findNotebyId,
