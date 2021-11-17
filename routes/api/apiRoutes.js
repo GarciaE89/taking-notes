fc2079b3f950f96889fd829caac6ccc9f242b4d3
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-11-17 00:07:14 -0700

    api routes, and changes to index.js

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -4,3 +4,44 @@ const path = require('path');
 const { findNotebyId, createNote, validateNotes } = require('./noteFunctions');
 const router = require('express').Router();
 
+var getUid = require('get-uid');
+
+// data
+const  notesArr  = require('../../db/db.json');
+
+//get route for all notes
+router.get('/data', (req, res) => {
+  let allNotes = fs.readFileSync(path.join(__dirname, "../../db/db.json"));
+  allNotes = JSON.parse(allNotes)
+  res.json(allNotes)
+});
+
+//get note by id
+router.delete('/data/:id', (req, res) => {
+  let id = req.params.id;
+  let newArray = fs.readFileSync(path.join(__dirname, "../../db/db.json"));
+  newArray = JSON.parse(newArray);
+  console.log(newArray);
+  let result = findNotebyId(id, newArray);
+  res.json(result);
+});
+
+// new note
+router.post('/data', (req, res) => {
+  
+  req.body.id = getUid();
+  
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNotes(req.body)) {
+    res.status(400).send('400 bad request');
+  } else {
+    
+    // add note to json file and notesArray in this function
+    const note = createNote(req.body, notesArr);
+
+    // res.json(req.body);
+    res.json(note);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require('express');
 // lets heroku know which port to use
 const PORT = process.env.PORT || 3001;
 
-const app = express;
+const app = express();
 
 // route modules trying to organize by concerns
 const apiRoutes = require('./routes/api/apiRoutes');
 const htmlRoutes = require('./routes/html/htmlRoutes');
 
 // middleware serves static content from public dir.
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 
 app.use(express.urlencoded({ extended: true }));
